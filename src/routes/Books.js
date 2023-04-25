8285c62394e3d8b4b1fb10528b9fe92e22a812cf
darkrose56 <manishakc2056@gmail.com>
2023-04-26 00:59:01 +0545

    as

--- src/routes/Books.js
+++ src/routes/Books.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 
 
 
+
 function Books() {
     let checkout = new KhaltiCheckout(config);   
     const handlePaid = async () => {
@@ -178,7 +179,7 @@ function Books() {
                     <div class="col-md-6 pb-4">
                         <div class="d-flex">
                             <select class="form-control">
-                                <option>{localStorage.getItem('paid')}</option>
+                                <option>All</option>
                                 <option>Premium</option>
                                 <option>Free</option>
                             </select>
@@ -235,6 +236,8 @@ function Books() {
                                            else{
                                             if(product.flash_product==1&& localStorage.getItem("paid")!="1"){
                                                 checkout.show({amount: 1000});
+                                                handlePaid();
+                                                localStorage.setItem("paid" ,"1");
                                             }
                                             else {
                                             navigate('/product-details',{state:{product}});
@@ -296,108 +299,7 @@ function Books() {
     {/* <!-- End Content -->
 
     <!-- Start Brands --> */}
-    <section class="bg-light py-5">
-        <div class="container my-4">
-            <div class="row text-center py-3">
-                <div class="col-lg-6 m-auto">
-                    <h1 class="h1">Our Brands</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        Lorem ipsum dolor sit amet.
-                    </p>
-                </div>
-                <div class="col-lg-9 m-auto tempaltemo-carousel">
-                    <div class="row d-flex flex-row">
-                        {/* <!--Controls--> */}
-                        <div class="col-1 align-self-center">
-                            <a class="h1" href="#multi-item-example" role="button" data-bs-slide="prev">
-                                <i class="text-light fas fa-chevron-left"></i>
-                            </a>
-                        </div>
-                        {/* <!--End Controls-->
 
-                        <!--Carousel Wrapper--> */}
-                        <div class="col">
-                            <div class="carousel slide carousel-multi-item pt-2 pt-md-0" id="multi-item-example" data-bs-ride="carousel">
-                                {/* <!--Slides--> */}
-                                <div class="carousel-inner product-links-wap" role="listbox">
-
-                                    {/* <!--First slide--> */}
-                                    <div class="carousel-item active">
-                                        <div class="row">
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_01.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_02.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_03.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_04.png" alt="Brand Logo"/></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* <!--End First slide-->
-
-                                    <!--Second slide--> */}
-                                    <div class="carousel-item">
-                                        <div class="row">
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_01.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_02.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_03.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_04.png" alt="Brand Logo"/></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* <!--End Second slide-->
-
-                                    <!--Third slide--> */}
-                                    <div class="carousel-item">
-                                        <div class="row">
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_01.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_02.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_03.png" alt="Brand Logo"/></a>
-                                            </div>
-                                            <div class="col-3 p-md-5">
-                                                <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_04.png" alt="Brand Logo"/></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* <!--End Third slide--> */}
-
-                                </div>
-                                {/* <!--End Slides--> */}
-                            </div>
-                        </div>
-                        {/* <!--End Carousel Wrapper--> */}
-
-                        {/* <!--Controls--> */}
-                        <div class="col-1 align-self-center">
-                            <a class="h1" href="#multi-item-example" role="button" data-bs-slide="next">
-                                <i class="text-light fas fa-chevron-right"></i>
-                            </a>
-                        </div>
-                        {/* <!--End Controls--> */}
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </section>
     {/* <!--End Brands--> */}
     <Footer/>
 
